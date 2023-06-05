2b541d5647cd33bbd5445b5dc6421fad4cf95519
Peter Braun <peter-oskar.braun@informatik.hs-fulda.de>
2023-06-05 12:53:11 +0200

    implemented components + heroicons

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,16 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./index.css";
+
+import MyButton from "./partials/myButton";
+import MyHeader from "./partials/myHeader";
+
+//*
+//<h1 className="text-2xl font-bold underline">Hello world!</h1>
+//<h1>Users</h1>
+//{this.state.users.map((user) => (
+//  <div key={user.id}>{user.username}</div>
+//))}
+//
 
 class App extends Component {
   state = { users: [] };
@@ -13,11 +24,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="text-4xl font-bold underline">Hello world!</h1>
-        <h1>Users</h1>
-        {this.state.users.map((user) => (
-          <div key={user.id}>{user.username}</div>
-        ))}
+        <MyHeader></MyHeader>
       </div>
     );
   }
